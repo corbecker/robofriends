5e1c91aa6c8642131b4be182fe530f653260396e
corbecker <corbinbecker@protonmail.com>
2019-01-21 15:05:54 +0000

    reducer provider connect added

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,7 +9,7 @@ import { setSearchField } from '../actions';
 
 const mapStateToProps = state => {
   return {
-    searchfield: state.searchRobots.searchField
+    searchField: state.searchField
   }
 }
 
@@ -24,8 +24,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: [],
-      searchfield: ''
+      robots: []
     }
   }
 
@@ -35,21 +34,18 @@ class App extends Component {
       .then(users => {this.setState( {robots: users})});
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value});
-  }
-
   render(){
-    const { robots, searchfield } = this.state;//destructuring
+    const { robots } = this.state;//destructuring
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
       return !robots.length ?
         <h1>Loading...</h1> :
    (
         <div className="tc">
         <h1 className="f1">RoboFriends</h1>  
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundary>
             <Cardlist robots={filteredRobots} />
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { searchRobots } from './reducers.js';
 
