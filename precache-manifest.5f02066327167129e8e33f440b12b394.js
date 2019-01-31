512af486234c0c2f787fff6fe3118c4198d0dc0c
corbecker <corbinbecker@protonmail.com>
2019-01-31 08:57:30 +0000

    Updates

---
+++ precache-manifest.5f02066327167129e8e33f440b12b394.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "d5da6fa169ff10e7b440",
+    "url": "/robofriends/static/js/main.d5da6fa1.chunk.js"
+  },
+  {
+    "revision": "82562b34a8f075326631",
+    "url": "/robofriends/static/js/1.82562b34.chunk.js"
+  },
+  {
+    "revision": "d5da6fa169ff10e7b440",
+    "url": "/robofriends/static/css/main.bc88fe93.chunk.css"
+  },
+  {
+    "revision": "82562b34a8f075326631",
+    "url": "/robofriends/static/css/1.53228976.chunk.css"
+  },
+  {
+    "revision": "0791bc25dcc4cb2006e5818e73650682",
+    "url": "/robofriends/index.html"
+  }
+];
